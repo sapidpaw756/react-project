aebc461ac5c49c94675d4d139fa48f47d9cf837c
Alfie Patnugot <alfie.s.patnugot@accenture.com>
2024-06-17 14:09:00 +0800

    push experience

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,13 @@ import Header from './template/Header';
 import Home from './template/Home';
 import About from './template/About';
 import Skills from './template/Skills';
+import Experience from './template/Experience';
 import './scss/core.scss';
 import './scss/helpers.scss';
 
 function App() {
   return (
-    [<Header/>, <Home/>, <About/>, <Skills/>]
+    [<Header/>, <Home/>, <About/>, <Skills/>, <Experience/>]
   );
 }
 
--- portfolio/src/template/About.js
+++ portfolio/src/template/About.js
@@ -20,7 +20,7 @@ const About = () => {
                     <p className="text-center fs-60">Hi! Thanks for dropping by.</p>
                     <p className="fs-20 p-l-lg-30 text-center">My name is Alfie, I'm {<Age/>} years young, happily married, no kids yet but hey, we're working on it!<br /><br />
                     On my free time, I enjoy watching anime series, reading manga and surfing the net. I'm not really the athletic type but you can count on me for most online games. <br/><br />
-                    I have been a software engineer for various companies for a couple of years now, doing bck end support in my early years and recently, a lot of front-end activities. feel free to explore my Portfolio to learn more bout my work experience. <br/><br />
+                    I have been a software engineer for various companies for a couple of years now, doing back end support in my early years and recently, a lot of front-end activities. feel free to explore my Portfolio to learn more bout my work experience. <br/><br />
                     He'res to hoping I get shortlisted for your Company! Cheers!</p>
                 </div>
             </div>

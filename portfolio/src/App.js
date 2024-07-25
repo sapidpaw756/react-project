a59ea68c1f47ef25996e9a84667c343e29fc0ae8
Alfie <alfiepatnugot@gmail.com>
2024-07-25 16:14:04 +0800

    update contact

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,9 +8,14 @@ import Contact from './template/Contact';
 import './scss/core.scss';
 import './scss/helpers.scss';
 
+
+const pages =[{id:0, page: Header}, {id:1, page: Home}, {id:2, page: About}, {id:3, page: Skills}, {id:4, page: Experience} ,{id:5, page: Contact}]
+
 function App() {
   return (
-    [<Header/>, <Home/>, <About/>, <Skills/>, <Experience/>, <Contact/>]
+    [pages.map(p => <div key={p.id}>
+      <p.page/>
+    </div>)]
   );
 }
 
--- portfolio/src/template/Contact.js
+++ portfolio/src/template/Contact.js
@@ -1,11 +1,34 @@
 import '../scss/contact.scss';
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Modal from 'react-modal';
 
 const Contact = () => {
-    const [input, setInput] = useState(""),
+ 
+    const form = useRef(),
+    [input, setInput] = useState(""),
+    [modalIsOpen, setIsOpen] = React.useState(false),
     emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     let timer,
-    timeoutVal = 1000;
+    timeoutVal = 1000,
+    customStyles = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+      },
+    };
+
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
 
     const validateEmail = (e) => {
       const email = e.target.value;
@@ -14,43 +37,84 @@ const Contact = () => {
       timer = window.setTimeout(() => {
         const isValidEmail = emailRegex.test(email);
         const invalid = document.querySelector('.invalid-email');
-    
+        const required = document.querySelector('.required-email');
         if(isValidEmail){
           invalid.classList.add('d-none');
         }
         else{
           invalid.classList.remove('d-none');
         }
+        if(email.length > 0){
+          required.classList.add('d-none');
+        }
         setInput(email);
       }, timeoutVal);
     
     }
+
     const handleSubmit = (e) => {
       e.preventDefault();
+      const required = document.querySelector('.required-email');
+
+      if(sessionStorage.getItem("sentEmail")){
+        openModal();
+        return;
+      }
+
       if(input && input.length && input.match(emailRegex)){
-        console.log('true');
+        required.classList.add('d-none');
+        const templateId = 'template_kw6hwqj';
+        const serviceId = 'service_3twr177';
+
+        var templateParams = {
+          name: 'James',
+          notes: 'Check this out!',
+        };
+        sendFeedback(serviceId,templateId, templateParams)
+      }
+      else{
+        required.classList.remove('d-none');
       }
     };
+
+    function sendFeedback (serviceId, templateId, variables)  {
+      emailjs
+      .sendForm(serviceId, templateId, form.current, {
+        publicKey: 'GxXYFeKvaLEc760OF',
+      })
+      .then(
+        () => {
+          sessionStorage.setItem("sentEmail", "1");
+          document.querySelector(".form").reset();;
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    }
+
     return (
-      <div id="Contact" className="page-section contact-section p-t-80 p-b-80 s-m-t-80">
+      
+      <div id="Contact" className="page-section contact-section p-t-80 p-b-80 s-m-t-65">
         <div className="content">
             <div className="row">
               <div className="col-12 d-flex">
                 <div className="form-container">
                   <h3 className="fs-30 fs-lg-60">Contact Me!</h3>
-                  <form className="form m-t-40 d-flex flex-column">
+                  <form ref={form} className="form m-t-40 d-flex flex-column">
                     <label className="d-flex flex-column">
                       <span className="fs-18 m-b-10"><b>Your Name</b></span>
-                      <input type="text" placeholder="What's your name?"/>
+                      <input type="text" placeholder="What's your name?" name="user_name"/>
                     </label>
                     <label className="d-flex flex-column">
                       <span className="fs-18 m-b-10"><b>Your Email</b></span>
-                      <input type="email" onKeyUp={validateEmail} placeholder="What's your email?" required/>
+                      <input type="email" name="user_email" onKeyUp={validateEmail} placeholder="What's your email?" required/>
                       <span className="invalid-email d-none">Invalid Email!!!</span>
+                      <span className="required-email d-none">Required Email!!!</span>
                     </label>
                     <label className="d-flex flex-column">
                       <span className="fs-18 m-b-10"><b>Your message</b></span>
-                      <textarea  placeholder="What's your message? (optional)"/>
+                      <textarea name="message" placeholder="What's your message? (optional)"/>
                     </label>
                     <button
                     className='btn-submit'
@@ -64,6 +128,21 @@ const Contact = () => {
               </div>
             </div>
         </div>
+        <Modal
+          appElement={document.getElementById('root') || undefined}
+          isOpen={modalIsOpen}
+          onClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div className='btn-container'>
+            <button className="close-btn" onClick={closeModal}></button>
+          </div>
+          <div>
+            <h2>Hello,</h2>
+            <p>Only 1 request per session.</p>
+          </div>
+        </Modal>
       </div>
     );
 }
